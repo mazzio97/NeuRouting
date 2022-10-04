7e21792a49bb3e41d97e7cbcf38e69addd212ad3
n28div <nicolaslazzari98@gmail.com>
2022-10-04 17:57:19 +0200

    fix lkh solver for multithreading

--- src/baselines/lkh_solver.py
+++ src/baselines/lkh_solver.py
@@ -14,38 +14,37 @@ class LKHSolver(VRPSolver):
         self.executable = executable
 
     def reset(self, instance: VRPInstance):
-        self.instance = instance
+        pass
 
     def initial_solution(self, instance: VRPInstance):
         return self.solve(instance, max_steps=1)
 
-    def solve(self, instance: VRPInstance, max_steps=None, runs=None, time_limit=None):
+    def solve(self, instance: VRPInstance, max_steps=None, runs=10, time_limit=None):
         # assert time_limit is None, "LKH3 does not provide any time limitation parameter"
-        self.reset(instance)
         if max_steps is None:
-            max_steps = self.instance.n_customers
+            max_steps = instance.n_customers
         with tempfile.TemporaryDirectory() as tempdir:
             problem_filename = os.path.join(tempdir, "problem.vrp")
             output_filename = os.path.join(tempdir, "output.tour")
             param_filename = os.path.join(tempdir, "params.par")
 
-            write_vrp(self.instance, problem_filename)
+            write_vrp(instance, problem_filename)
             params = {"PROBLEM_FILE": problem_filename,
                       "OUTPUT_TOUR_FILE": output_filename,
-                      "MAX_TRIALS": max_steps,
-                      "RUNS": runs}
+                      "MAX_TRIALS": max_steps}
+                      #"RUNS": runs}
             self.write_lkh_par(param_filename, params)
             check_output([self.executable, param_filename])
-            tours = read_solution(output_filename, self.instance.n_customers)
-            tours = [Route(t, self.instance) for t in tours]
-            self.solution = VRPSolution(self.instance, tours)
-        return self.solution
+            tours = read_solution(output_filename, instance.n_customers)
+            tours = [Route(t, instance) for t in tours]
+            solution = VRPSolution(instance, tours)
+        return solution
 
     @staticmethod
     def write_lkh_par(filename, parameters):
         default_parameters = {  # Use none to include as flag instead of kv
             "SPECIAL": None,
-            "RUNS": 1,
+            "RUNS": 10,
             "TRACE_LEVEL": 1,
             "SEED": 0
         }
