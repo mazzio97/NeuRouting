71b84a07e288e3eca4c4fdd504886ef0931ff923
Francesco <francesco.mistri98@gmail.com>
2023-03-14 19:12:54 +0100

    Transitioned GreedyRepair to the new hierarchy
    + added tests.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,8 @@
 import pytest
 
 from context import nlns
+from nlns.generators import generate_multiple_instances
+from nlns.instances import VRPSolution
 
 
 @pytest.fixture
@@ -9,3 +11,11 @@ def set_default_rng():
     nlns.default_rng.seed(17170)
     yield
     nlns.default_rng.setstate(previous_state)
+
+
+@pytest.fixture(params=[20, 50, 100])
+def empty_solutions(request):
+    # TODO: Handle seeds in generate_multiple_instances
+
+    return tuple(map(VRPSolution,
+                     generate_multiple_instances(50, request.param)))
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,8 +1,12 @@
+import copy
+from operator import attrgetter
+
 import pytest
 
 from context import nlns
 from nlns.operators import LNSOperator
-from helpers import set_default_rng                         # NOQA
+from nlns.operators.repair import GreedyRepair
+from helpers import set_default_rng, empty_solutions        # NOQA
 
 
 class IdentityOperator(LNSOperator):
@@ -26,3 +30,33 @@ class TestLNSOperator:
                               set_default_rng, seed=1):         # NOQA
         identity_operator.set_random_state(seed)
         assert identity_operator.rng.getstate() != nlns.default_rng.getstate()
+
+
+class TestRepair:
+
+    @pytest.mark.parametrize('operator_type, seed', [(GreedyRepair, 42)])
+    def test_repair(self, operator_type, seed, empty_solutions):        # NOQA
+        operator = operator_type()
+
+        operator.set_random_state(seed)
+        solutions = operator(empty_solutions)
+
+        assert all(not solution.missing_customers() for solution in solutions)
+
+    @pytest.mark.parametrize('operator_type, seed', [(GreedyRepair, 42)])
+    def test_reproducibility(self, operator_type, seed,
+                             empty_solutions):                  # NOQA
+        operator = operator_type()
+
+        empty_copy = copy.deepcopy(empty_solutions)
+
+        operator.set_random_state(seed)
+        solutions = operator(empty_solutions)
+
+        operator.set_random_state(seed)
+        solutions_copy = operator(empty_copy)
+
+        costs = list(map(attrgetter('cost'), solutions))
+        costs_copy = list(map(attrgetter('cost'), solutions_copy))
+
+        assert costs == costs_copy
