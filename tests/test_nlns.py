496ca769acc7a94fa8a82472c11c5329c9efe404
Francesco <francesco.mistri98@gmail.com>
2023-03-14 17:01:59 +0100

    Add tests for module_found(...)

--- tests/test_nlns.py
+++ tests/test_nlns.py
@@ -1,5 +1,18 @@
 import pytest
 
+from context import nlns
+
 
 def test_tests():
     assert True
+
+
+@pytest.mark.parametrize('module_name, exception',
+                         [('random', None), ('xxxxxx', ModuleNotFoundError)])
+def test_module_found(module_name, exception, current_module='yyyyyy'):
+    if exception is None:
+        nlns.module_found(module_name, current_module)
+        return
+
+    with pytest.raises(exception):
+        nlns.module_found(module_name, current_module)
