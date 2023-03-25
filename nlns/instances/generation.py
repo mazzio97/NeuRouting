c05e02b07e2e0d32e7e02fb016f390bde93d0d36
Francesco <francesco.mistri98@gmail.com>
2023-03-25 19:44:09 +0100

    Add lazy generation to generate_instances(...)
    A "preload chunk size" can be specified via the "preload" attribute. This is an upper bound for the amount of instances generated in the same call, leading overall to a maximum memory footprint of "preload" number of instances.

--- nlns/instances/generation.py
+++ nlns/instances/generation.py
@@ -1,5 +1,4 @@
-from typing import Sequence, Union, Iterable, Callable
-from itertools import chain
+from typing import Sequence, Union, Iterable, Callable, Optional
 
 import numpy as np
 
@@ -18,9 +17,13 @@ distributions = {'nazari': generate_nazari_instances,
 def generate_instances(n_instances: int,
                        n_customers: Union[int, Sequence[int]],
                        distribution: Union[str, InstancesGenerator] = 'nazari',
-                       seed: nlns.RandomSeedOrState = None
+                       seed: nlns.RandomSeedOrState = None,
+                       preload: Optional[int] = None
                        ) -> Iterable[VRPInstance]:
     """Generate instances from a given distribution.
+
+    By specifying a ``preload`` size it is possible to obtain lazy
+    generation.
 
     Args:
         n_instances: Total number of instances to generate.
@@ -36,6 +39,17 @@ def generate_instances(n_instances: int,
             ``uchoa``.
         seed: A seed or random state for reproducible instances
             generation.
+        preload: Any value greater than 0 will determine, at most,
+            how many instances will be pregenerated. Tuning this value
+            can lead to efficient online generation without filling
+            memory.
+
+            In general the greater the value, the faster the generation
+            and the higher the memory consumption.
+
+            Default (``None``) leads to pregeneration of as much
+            instances as possible. This is the fastest solution for a
+            small number of instances.
     """
     if type(n_customers) is int:
         n_customers = n_customers,
@@ -44,16 +58,33 @@ def generate_instances(n_instances: int,
     if type(distribution) is str:
         instance_generator = distributions[distribution]
 
+    if preload is None:
+        preload = n_instances
+
     # https://stackoverflow.com/questions/48918627/split-an-integer-into-bins
-    instances_per_customer = np.arange(n_instances + len(n_customers) - 1,
-                                       n_instances - 1, -1) // len(n_customers)
+    instances_per_chunk = np.arange(n_instances + len(n_customers) - 1,
+                                    n_instances - 1, -1) // len(n_customers)
 
     rng = nlns.get_rng(seed)
-    seeds_per_customer = tuple(rng.random() for _ in n_customers)
 
-    return (chain(*(instance_generator(i, c, s)
-                    for i, c, s in zip(instances_per_customer, n_customers,
-                                       seeds_per_customer))))
+    if n_instances <= 0:
+        return
+
+    for chunk_instances, customers in zip(instances_per_chunk, n_customers):
+        # Divide per customer chunks into subchunks, eventually based
+        # on preload size
+        n_to_generate = min(chunk_instances, preload)
+
+        # Compute the total number of subchunks and an eventual
+        # remaining
+        full_preloads, partial_preload = divmod(chunk_instances, n_to_generate)
+        for _ in range(full_preloads):
+            yield from instance_generator(n_to_generate, customers,
+                                          rng.random())
+
+        if partial_preload > 0:
+            yield from instance_generator(partial_preload, customers,
+                                          rng.random())
 
 
 def generate_instance(n_customers: int,
