4433be065a5cec569efaba60a78cace1343ea2b1
Francesco <francesco.mistri98@gmail.com>
2023-04-01 22:30:20 +0200

    Slight restyle of ORToolsSolver
    Still cannot figure out how to seed it.

--- nlns/baselines/ortools.py
+++ nlns/baselines/ortools.py
@@ -1,21 +1,23 @@
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
-from nlns.environments import VRPSolver
-from nlns.instances import VRPSolution, VRPInstance
-from nlns.instances.vrp_solution import Route
-from nlns.utils.vrp_io import read_vrp, GRID_DIM
+import matplotlib.pyplot as plt
+
+from nlns.instances import VRPSolution, VRPInstance, Route, generate_instance
+from nlns.io import GRID_DIM
 
 
-class ORToolsSolver(VRPSolver):
+class ORToolsSolver:
+
     def __init__(self):
-        super().__init__("ortools")
+        # super().__init__("ortools")
         self._data = None
         self._manager = None
         self._routing = None
 
     def reset(self, instance: VRPInstance):
         self.instance = instance
-        n_vehicles = instance.n_customers  # int(sum(instance.demands) // instance.capacity) + 2
+        # int(sum(instance.demands) // instance.capacity) + 2
+        n_vehicles = instance.n_customers
         self._data = {
             'distance_matrix': instance.distance_matrix * GRID_DIM,
             'demands': [0] + instance.demands,
@@ -23,26 +25,33 @@ class ORToolsSolver(VRPSolver):
             'vehicle_capacities': [instance.capacity] * n_vehicles,
             'depot': 0
         }
-        self._manager = pywrapcp.RoutingIndexManager(instance.n_customers + 1, n_vehicles, self._data['depot'])
+        self._manager = pywrapcp.RoutingIndexManager(
+            instance.n_customers + 1, n_vehicles, self._data['depot'])
         self._routing = pywrapcp.RoutingModel(self._manager)
-        transit_callback_index = self._routing.RegisterTransitCallback(self._distance_callback)
+        transit_callback_index = self._routing.RegisterTransitCallback(
+            self._distance_callback)
         self._routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-        demand_callback_index = self._routing.RegisterUnaryTransitCallback(self._demand_callback)
-        self._routing.AddDimensionWithVehicleCapacity(evaluator_index=demand_callback_index,
-                                                      slack_max=0,  # null capacity slack
-                                                      vehicle_capacities=self._data['vehicle_capacities'],
-                                                      fix_start_cumul_to_zero=True,  # start cumulative to zero
-                                                      name='Capacity')
+        demand_callback_index = self._routing.RegisterUnaryTransitCallback(
+            self._demand_callback)
+        self._routing.AddDimensionWithVehicleCapacity(
+            evaluator_index=demand_callback_index,
+            slack_max=0,  # null capacity slack
+            vehicle_capacities=self._data['vehicle_capacities'],
+            fix_start_cumul_to_zero=True,  # start cumulative to zero
+            name='Capacity')
 
-    def solve(self, instance: VRPInstance, time_limit=None, max_steps=None) -> VRPSolution:
+    def solve(self, instance: VRPInstance, time_limit=None,
+              max_steps=None) -> VRPSolution:
         self.reset(instance)
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         if time_limit is not None:
             search_parameters.time_limit.FromSeconds(time_limit)
         if max_steps is not None:
             search_parameters.solution_limit = max_steps
-        search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
-        search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+        search_parameters.first_solution_strategy = \
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+        search_parameters.local_search_metaheuristic = \
+            routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
 
         solution = self._routing.SolveWithParameters(search_parameters)
         self.solution = self._process_solution(solution)
@@ -77,7 +86,8 @@ class ORToolsSolver(VRPSolver):
 
 
 if __name__ == "__main__":
-    inst = read_vrp("../../res/A-n32-k5.vrp", grid_dim=100)
+    inst = generate_instance(50, seed=1)
     or_solver = ORToolsSolver()
-    or_solver.solve(inst, time_limit=10)
-    or_solver.render()
+    solution = or_solver.solve(inst, time_limit=10)
+    solution.plot()
+    plt.show()
